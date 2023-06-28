03c4c54f1df348c9e2a347be26cd3e77b751f07b
ungebrimi <devjoakimpedersen@gmail.com>
2023-06-28 22:18:29 +0200

    just a start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from './layout/Navbar'
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +12,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='h-full bg-gray-50'>
+      <body className="h-full">
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
