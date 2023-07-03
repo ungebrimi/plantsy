d625621c9702216f859eeb7b3de525630da7f384
ungebrimi <devjoakimpedersen@gmail.com>
2023-07-03 18:15:50 +0200

    homepage scetch

---
+++ app/components/home/Features.tsx
@@ -0,0 +1,65 @@
+import { InboxIcon, TrashIcon, UsersIcon } from '@heroicons/react/24/outline'
+const features = [
+
+
+  {
+    name: 'Post a job and hire a pro',
+    description:
+      'Non quo aperiam repellendus quas est est. Eos aut dolore aut ut sit nesciunt. Ex tempora quia. Sit nobis consequatur dolores incidunt.',
+    href: '#',
+    icon: InboxIcon,
+  },
+  {
+    name: 'Browse and buy projects',
+    description:
+      'Vero eum voluptatem aliquid nostrum voluptatem. Vitae esse natus. Earum nihil deserunt eos quasi cupiditate. A inventore et molestiae natus.',
+    href: '#',
+    icon: UsersIcon,
+  },
+  {
+    name: 'Get advice from an industry expert',
+    description:
+      'Et quod quaerat dolorem quaerat architecto aliquam accusantium. Ex adipisci et doloremque autem quia quam. Quis eos molestiae at iure impedit.',
+    href: '#',
+    icon: TrashIcon,
+  },
+]
+
+export default function Features() {
+  return (
+    <div className="w-full py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Find proffesionals your way
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Work with the largest network of independent professionals and get things done—from quick turnarounds to big transformations.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+            {features.map((feature) => (
+              <div key={feature.name} className="flex flex-col">
+                <dt className="text-base font-semibold leading-7 text-gray-900">
+                  <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-green-600">
+                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                  <p className="flex-auto">{feature.description}</p>
+                  <p className="mt-6">
+                    <a href={feature.href} className="text-sm font-semibold leading-6 text-green-600">
+                      Learn more <span aria-hidden="true">→</span>
+                    </a>
+                  </p>
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import Image from 'next/image'
 import Hero from './components/home/Hero'
 import Categories from './components/home/Categories'
+import Features from './components/home/Features'
+import Testimonials from './components/home/Testimonials'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Hero />
       <Categories />
+      <Features />
+      <Testimonials />
     </main>
   )
 }
