e7afce1b1dfa10132add305f6748a294764be7ee
joakim <devjoakimpedersen@gmail.com>
2023-12-01 12:22:35 +0100

    filter on discover section, pushing to see how it works online

--- app/discover/ClientPage.tsx
+++ app/discover/ClientPage.tsx
@@ -32,7 +32,7 @@ const ClientPage = ({
 
         <section aria-labelledby="products-heading" className="pb-24 pt-6">
           <h2 id="products-heading" className="sr-only">
-            Discover offered services
+            Discover offered services near you
           </h2>
 
           <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
